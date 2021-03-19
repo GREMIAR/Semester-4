5c78c6b609d981281cf7f2cd0f542eb84c6d08a7
GREBIAR-Git <nikitos.muzalevskiY@mail.ru>
2021-03-19 19:54:27 +0300

    temporary

---
+++ Lab1/ClassLibrary/ArgsWriter/ArgsWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArgsWriter
+{
+    public class ArgsWriter
+    {
+        public static void ArgsOutput(string [] args)
+        {
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
---
+++ Lab1/Main/lab1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ArgsWriter;
+namespace PiAPS_labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input="";
+            while (input!="9"){
+                Console.Write("\n1. Вывод на экран аргументов, переданные в программу при запуске в командной строке. \n2. Вывод високосных лет с 1900 по 2000 гг\n3. Вывод последовательность чисел Фибоначи до заданного числа.\n4. Вычисление факториала заданного числа.\n5. Вывод всеч простых чисел не превышающие заданное.\nВвод: ");
+                input=Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                    {
+                        ArgsWriter.ArgsOutput(args);
+                        //ArgsWriter.ArgsWriter1(args);
+                        break;
+                    }   
+                    /*case "2":
+                    {
+                        FindLeapYear(1900,2000);
+                        break;
+                    }
+                    case "3":
+                    {
+                        input=Console.ReadLine();
+                        Fibonacci(ulong.Parse(input));
+                        break;
+                    }
+                    case "4":
+                    {
+                        input=Console.ReadLine();
+                        Factorial(ulong.Parse(input));
+                        break;
+                    }
+                    case "5":
+                    {
+                        input=Console.ReadLine();
+                        Eratosthenes(int.Parse(input));
+                        break;
+                    }*/
+                    default:
+                        break;
+                } 
+            }
+        }
+    }
+}
