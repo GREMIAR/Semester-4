09a47dbfc692cf71d67843179a19de7e3cd69e2f
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-03-22 02:59:05 +0300

    del unnecessary..............

---
+++ Lab2-3/ClientInterface/ClientInterface/MainWindow.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ArchitectureLab2
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxUserList = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonLogIn.Location = new System.Drawing.Point(6, 207);
+            this.buttonLogIn.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(427, 55);
+            this.buttonLogIn.TabIndex = 1;
+            this.buttonLogIn.Text = "Вход";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxName.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxName.Location = new System.Drawing.Point(6, 140);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(427, 44);
+            this.textBoxName.TabIndex = 2;
+            this.textBoxName.Text = " Имя";
+            this.textBoxName.Click += new System.EventHandler(this.textBoxName_Click);
+            this.textBoxName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxName_KeyDown);
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMsg.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMsg.Location = new System.Drawing.Point(445, 1218);
+            this.textBoxMsg.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxMsg.Multiline = true;
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMsg.Size = new System.Drawing.Size(867, 127);
+            this.textBoxMsg.TabIndex = 3;
+            this.textBoxMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMsg_KeyDown);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSend.Location = new System.Drawing.Point(1324, 1218);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(428, 127);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxUserList
+            // 
+            this.textBoxUserList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxUserList.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxUserList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxUserList.Location = new System.Drawing.Point(6, 339);
+            this.textBoxUserList.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxUserList.Multiline = true;
+            this.textBoxUserList.Name = "textBoxUserList";
+            this.textBoxUserList.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.textBoxUserList, 2);
+            this.textBoxUserList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxUserList.Size = new System.Drawing.Size(427, 1006);
+            this.textBoxUserList.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 268);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(427, 65);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Пользователи онлайн";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPort.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPort.Location = new System.Drawing.Point(6, 73);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(427, 44);
+            this.textBoxPort.TabIndex = 12;
+            this.textBoxPort.Text = " Порт";
+            this.textBoxPort.Click += new System.EventHandler(this.textBoxPort_Click);
+            this.textBoxPort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPort_KeyDown);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxAddress.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textBoxAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxAddress.Location = new System.Drawing.Point(6, 6);
+            this.textBoxAddress.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(427, 44);
+            this.textBoxAddress.TabIndex = 10;
+            this.textBoxAddress.Text = " Адрес";
+            this.textBoxAddress.Click += new System.EventHandler(this.textBoxAddress_Click);
+            this.textBoxAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxAddress_KeyDown);
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxChat.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxChat, 2);
+            this.textBoxChat.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBoxChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxChat.Location = new System.Drawing.Point(445, 6);
+            this.textBoxChat.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxChat.Multiline = true;
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.ReadOnly = true;
+            this.tableLayoutPanel1.SetRowSpan(this.textBoxChat, 6);
+            this.textBoxChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxChat.Size = new System.Drawing.Size(1307, 1200);
+            this.textBoxChat.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.textBoxChat, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxAddress, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonLogIn, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSend, 2, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxMsg, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxUserList, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxName, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxPort, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 7);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.83871F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.05376F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1758, 1351);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1775, 1362);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MinimumSize = new System.Drawing.Size(387, 828);
+            this.Name = "ClientForm";
+            this.Text = "ChatClient";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxUserList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBoxChat;
+    }
+}
+
---
+++ Lab2-3/ClientInterface/ClientInterface/MainWindow.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ArchitectureLab2
+{
+    public partial class ClientForm : Form
+    {
+        static TcpClient client = null;
+        static string userName;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+            {
+            "8888"
+            };
+            textBoxPort.AutoCompleteCustomSource = source;
+            textBoxPort.AutoCompleteMode = AutoCompleteMode.Append;
+            textBoxPort.AutoCompleteSource = AutoCompleteSource.CustomSource;
+        }
+
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            LogIn();
+        }
+
+        public void LogIn()
+        {
+            try
+            {
+                userName = textBoxName.Text;
+                client = new TcpClient(textBoxAddress.Text, int.Parse(textBoxPort.Text));
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.Unicode.GetBytes(String.Format(userName + ": вошёл в чат "));
+                stream.Write(data, 0, data.Length);
+                Thread ReceThread = new Thread(Receive);
+                ReceThread.Start();
+                buttonSend.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show(
+                "Мы не можем установить надёжного соединения",
+                "Сообщение",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            }
+        }
+
+        public void Receive()
+        {
+            NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                byte[] data = new byte[1024];
+                int bytes = stream.Read(data, 0, data.Length);
+                string message = Encoding.Unicode.GetString(data, 0, bytes);
+                if (!OnlineClient(message))
+                {
+                    textBoxChat.AppendText(message);
+                    textBoxChat.AppendText(Environment.NewLine);
+                }
+            }
+        }
+        public bool OnlineClient(string user)
+        {
+            if (user[0] == '/')
+            {
+                user = user.Substring(5);
+                string[] words = user.Split('#');
+                textBoxUserList.Clear();
+                foreach (var word in words)
+                {
+                    textBoxUserList.AppendText(word);
+                    textBoxUserList.AppendText(Environment.NewLine);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Send()
+        {
+            NetworkStream stream = client.GetStream();
+            string message = textBoxMsg.Text;
+            textBoxChat.AppendText("Вы: " + message);
+            byte[] data = Encoding.Unicode.GetBytes(String.Format("{0}: {1}", userName, message));
+            stream.Write(data, 0, data.Length);
+            textBoxMsg.Clear();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void textBoxAddress_Click(object sender, EventArgs e)
+        {
+            textBoxAddress.Clear();
+        }
+
+        private void textBoxPort_Click(object sender, EventArgs e)
+        {
+            textBoxPort.Clear();
+        }
+
+        private void textBoxName_Click(object sender, EventArgs e)
+        {
+            textBoxName.Clear();
+        }
+
+        private void textBoxMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                Send();
+            }
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                string message = "/Close";
+                byte[] data = Encoding.Unicode.GetBytes(String.Format(message));
+                stream.Write(data, 0, data.Length);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void textBoxAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                ActiveControl = textBoxPort;
+            }
+        }
+
+        private void textBoxPort_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                ActiveControl = textBoxName;
+            }
+        }
+
+        private void textBoxName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                LogIn();
+            }
+        }
+    }
+}
