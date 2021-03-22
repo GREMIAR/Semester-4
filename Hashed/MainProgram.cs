f410255b6459fc3eedc0c120bb66e227069acaba
GREBIAR-Git <nikitos.muzalevskiY@mail.ru>
2021-03-22 23:11:09 +0300

    Hashed beginning

---
+++ Hashed/MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hashed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
