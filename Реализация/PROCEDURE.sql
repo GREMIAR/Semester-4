50304b4cd7caeca5b028db277a58c6baa33b068c
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-06-12 16:48:35 +0300

    1.1.2 (приписал запросы)

--- Реализация/PROCEDURE.sql
+++ Реализация/PROCEDURE.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 -- Товары определённого типа и производителя в неком городе
 CREATE PROCEDURE search_product(city VARCHAR(45), manufacturer VARCHAR(45), type  VARCHAR(45))
 BEGIN
-SELECT c.name, b.address,t.name, m.name, p.name, bp.quantity, bp.price
+SELECT c.name Город, b.street Улица, b.house Дом, t.name Тип, m.name Производитель, p.name Модель, bp.quantity Количество, bp.price Цена
 FROM city c
 JOIN branch b USING(city_id)
 JOIN branch_product bp USING(branch_id)
@@ -14,7 +14,7 @@ END$$
 -- Характеристики определённой модели товара
 CREATE PROCEDURE search_model_characteristics(model VARCHAR(45))
 BEGIN
-SELECT c.name, pc.value, pc.measure_units
+SELECT c.name Характкристика, pc.value Значение, c.measure_units Единица_измерения
 FROM product p
 JOIN product_characteristics pc USING(product_id)
 JOIN characteristics c USING(characteristics_id)
@@ -23,7 +23,7 @@ END$$
 -- все товары в неком городе
 CREATE PROCEDURE search_models_in_city(city VARCHAR(45))
 BEGIN
-SELECT p.name
+SELECT p.name Модель
 FROM branch b
 JOIN branch_product bp USING(branch_id)
 JOIN product p USING(product_id)
--- Реализация/SELECTFULL.sql
+++ Реализация/SELECTFULL.sql
@@ -3,13 +3,13 @@ CALL search_model_characteristics("Ryzen 7 3800XT");
 CALL search_models_in_city("Орёл");
 
 -- Количесво филиалов в городе
-SELECT c.name, COUNT(b.address) Филиалов
+SELECT c.name Город, COUNT(*) Филиалов
 FROM city c
 JOIN branch b USING(city_id)
 GROUP BY c.name;
 
 -- Прибыль от покупке
-SELECT s.sale_id, SUM((bp.price - p.price_purchases)*sp.quantity) Прибыль
+SELECT s.sale_id ID_Покупки, SUM((bp.price - p.price_purchases)*sp.quantity) Прибыль
 FROM sale s
 JOIN sale_product sp USING(sale_id)
 JOIN branch_product bp USING(branch_id,product_id)
@@ -17,13 +17,13 @@ JOIN product p USING(product_id)
 GROUP BY sale_id;
 
 -- Сотрудникои которы получат премию
-SELECT se.initials Фамилия_и_Инициалы,SUM(sa.total_cost) Продал_на
+SELECT CONCAT(se.lastname,LEFT(se.firstname,1),'.',LEFT(se.patronymic,1),'.') Фамилия_и_Инициалы, SUM(sa.total_cost) Продал_на
 FROM seller se
 JOIN sale sa USING(seller_id)
 WHERE MONTH(sa.date) = MONTH(curdate()) AND YEAR(sa.date) = YEAR(curdate())
 GROUP BY se.seller_id
 HAVING SUM(sa.total_cost)>100000
-ORDER BY se.initials;
+ORDER BY Фамилия_и_Инициалы;
 
 -- Сумма прибыли с покупок
 SELECT SUM(sa.total_cost) Сумма
