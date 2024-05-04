7c3b88de6b3e23d30f71aeecc4e435c64741cf0e
Grebiar <nikitos.muzalevskiy@mail.ru>
2024-05-04 04:18:27 +0300

    fix

--- PiAPS/PiAPS-practice/CommisVoyageur/CommisVoyageur/MainForm.cs
+++ PiAPS/PiAPS-practice/CommisVoyageur/CommisVoyageur/MainForm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,10 +6,10 @@ namespace CommisVoyageur
 {
     public partial class MainForm : Form
     {
-        Point unsavedPoint = new Point();
-        NearestNeighbor nearestNeighbor = new NearestNeighbor();
-        bool FreeSpace=true;
-        
+        readonly NearestNeighbor nearestNeighbor = new NearestNeighbor();
+        bool FreeSpace = true;
+        Point unsavedPoint;
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,26 +18,29 @@ namespace CommisVoyageur
             AreaPaint.Refresh();
         }
 
-        private void buttonAddPoint_Click(object sender, EventArgs e)
+        void buttonAddPoint_Click(object sender, EventArgs e)
         {
             SetBluePoint();
         }
 
-        private void comboBox_KeyPress(object sender, KeyPressEventArgs e)
+        void comboBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = true;
         }
 
-        private void MainForm_SizeChanged(object sender, EventArgs e)
+        void MainForm_SizeChanged(object sender, EventArgs e)
         {
             AreaPaint.Refresh();
         }
-        private void AreaPaint_Paint(object sender, PaintEventArgs e)
+
+        void AreaPaint_Paint(object sender, PaintEventArgs e)
         {
             CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(0, 0), new Point(AreaPaint.Width, 0));
-            CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(0, AreaPaint.Height), new Point(AreaPaint.Width, AreaPaint.Height));
+            CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(0, AreaPaint.Height),
+                new Point(AreaPaint.Width, AreaPaint.Height));
             CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(0, 0), new Point(0, AreaPaint.Height));
-            CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(AreaPaint.Width, 0), new Point(AreaPaint.Width, AreaPaint.Height));
+            CommisVoyageur.Paint.DrawLine(e, Color.Black, new Point(AreaPaint.Width, 0),
+                new Point(AreaPaint.Width, AreaPaint.Height));
             if (FreeSpace = nearestNeighbor.FreeSpaceAvailable(unsavedPoint))
             {
                 CommisVoyageur.Paint.DrawPoint(e, Color.Black, unsavedPoint);
@@ -47,37 +49,43 @@ namespace CommisVoyageur
             {
                 CommisVoyageur.Paint.DrawPoint(e, Color.Red, unsavedPoint);
             }
+
             foreach (Point point in nearestNeighbor.GetPoints())
             {
-                CommisVoyageur.Paint.DrawPoint(e, Color.Blue, point); 
+                CommisVoyageur.Paint.DrawPoint(e, Color.Blue, point);
             }
-            for(int i = 0; i < nearestNeighbor.GetpointsSorted().Count-1; i++)
+
+            for (int i = 0; i < nearestNeighbor.GetpointsSorted().Count - 1; i++)
             {
-                CommisVoyageur.Paint.DrawArrow(e, Color.Black, nearestNeighbor.GetpointsSorted()[i], nearestNeighbor.GetpointsSorted()[i+1]);
+                CommisVoyageur.Paint.DrawArrow(e, Color.Black, nearestNeighbor.GetpointsSorted()[i],
+                    nearestNeighbor.GetpointsSorted()[i + 1]);
             }
+
             if (nearestNeighbor.GetpointsSorted().Count > 2)
             {
-                CommisVoyageur.Paint.DrawArrow(e, Color.Black, nearestNeighbor.GetpointsSorted()[nearestNeighbor.GetpointsSorted().Count - 1], nearestNeighbor.GetpointsSorted()[0]);
+                CommisVoyageur.Paint.DrawArrow(e, Color.Black,
+                    nearestNeighbor.GetpointsSorted()[nearestNeighbor.GetpointsSorted().Count - 1],
+                    nearestNeighbor.GetpointsSorted()[0]);
                 CommisVoyageur.Paint.BigRedPoint(e, Color.Red, nearestNeighbor.GetpointsSorted()[0]);
             }
         }
 
-        private void AreaPaint_MouseClick(object sender, MouseEventArgs e)
+        void AreaPaint_MouseClick(object sender, MouseEventArgs e)
         {
-            textBox6.Text = string.Empty;
             unsavedPoint.X = Cursor.Position.X - PointToScreen(AreaPaint.Location).X - 11;
             unsavedPoint.Y = Cursor.Position.Y - PointToScreen(AreaPaint.Location).Y - 12;
             AreaPaint.Refresh();
-            if (e.Button==MouseButtons.Right)
+            if (e.Button == MouseButtons.Right)
             {
                 SetBluePoint();
             }
         }
 
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
             Search();
         }
+
         void SetBluePoint()
         {
             if (FreeSpace)
@@ -92,14 +100,14 @@ namespace CommisVoyageur
             {
                 toolStripStatusLabel1.Text = "Нельзя установить точку";
             }
-            textBox6.Text = string.Empty;
         }
+
         void Search()
         {
             if (!string.IsNullOrEmpty(comboBox3.Text) && nearestNeighbor.GetPoints().Count > 2)
             {
                 toolStripStatusLabel1.Text = string.Empty;
-                textBox6.Text = (Math.Round(nearestNeighbor.Greedy(int.Parse(comboBox3.Text) - 1), 2)).ToString();
+                textBox6.Text = Math.Round(nearestNeighbor.Greedy(int.Parse(comboBox3.Text) - 1), 2).ToString();
                 toolStripStatusLabel1.Text = "Поиск выполнен!";
                 AreaPaint.Refresh();
             }
--- PiAPS/PiAPS-practice/CommisVoyageur/CommisVoyageur/NearestNeighbor.cs
+++ PiAPS/PiAPS-practice/CommisVoyageur/CommisVoyageur/NearestNeighbor.cs
@@ -4,42 +4,44 @@ using System.Drawing;
 
 namespace CommisVoyageur
 {
-    class NearestNeighbor
+    internal class NearestNeighbor
     {
-        List<Point> points;
-        public List<Point> GetPoints()
-        {
-            return points;
-        }
-        public void AddPoints(Point point)
-        {
-            points.Add(point);
-        }
-        List<Point> pointsSorted;
-        public List<Point> GetpointsSorted()
-        {
-            return pointsSorted;
-        }
+        readonly List<Point> points;
+        readonly List<Point> pointsSorted;
 
         public NearestNeighbor()
         {
             points = new List<Point>();
             pointsSorted = new List<Point>();
         }
-        
+
+        public List<Point> GetPoints()
+        {
+            return points;
+        }
+
+        public void AddPoints(Point point)
+        {
+            points.Add(point);
+        }
+
+        public List<Point> GetpointsSorted()
+        {
+            return pointsSorted;
+        }
 
         public double Greedy(int indexStartPoint)
         {
             List<Point> tempPoints = new List<Point>(points);
             pointsSorted.Clear();
-            double minPath=0;
+            double minPath = 0;
             double minDistance;
             Point testPoint = tempPoints[indexStartPoint];
-            while (tempPoints.Count>0)
+            while (tempPoints.Count > 0)
             {
-                int delIndex=0;
+                int delIndex = 0;
                 minDistance = double.MaxValue;
-                for (int i=0; tempPoints.Count>i;i++)
+                for (int i = 0; tempPoints.Count > i; i++)
                 {
                     double distance = DistancePoint(testPoint, tempPoints[i]);
                     if (minDistance > distance)
@@ -48,30 +50,32 @@ namespace CommisVoyageur
                         delIndex = i;
                     }
                 }
+
                 testPoint = tempPoints[delIndex];
                 pointsSorted.Add(testPoint);
                 tempPoints.RemoveAt(delIndex);
                 minPath += minDistance;
             }
-            minPath += DistancePoint(pointsSorted[pointsSorted.Count-1], pointsSorted[0]);
-            return minPath;
 
+            minPath += DistancePoint(pointsSorted[pointsSorted.Count - 1], pointsSorted[0]);
+            return minPath;
         }
 
         double DistancePoint(Point first, Point second)
         {
-            return Math.Sqrt(Math.Pow(second.X - first.X, 2) + Math.Pow(second.Y - first.Y,2));
+            return Math.Sqrt(Math.Pow(second.X - first.X, 2) + Math.Pow(second.Y - first.Y, 2));
         }
 
         public bool FreeSpaceAvailable(Point unsavedPoint)
         {
             foreach (Point point in points)
             {
-                if ((Math.Pow(point.X - unsavedPoint.X, 2) + Math.Pow(point.Y - unsavedPoint.Y, 2) < 1600))
+                if (Math.Pow(point.X - unsavedPoint.X, 2) + Math.Pow(point.Y - unsavedPoint.Y, 2) < 1600)
                 {
                     return false;
                 }
             }
+
             return true;
         }
     }
